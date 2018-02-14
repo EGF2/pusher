e616854e0a30097f01330c1c1da057324376a091
Anton Shishenya <anton.shishenya@gmail.com>
2018-02-14 17:43:44 +0300

    Improve logging and modify services to accept env variables as config

--- components.js
+++ components.js
@@ -3,16 +3,31 @@
 const option = require("commons/option");
 const bunyan = require("bunyan");
 const sendgrid = require("sendgrid");
+require("dotenv").config();
 
 function init() {
     return option().config.then(config => {
-        module.exports.config = config;
-        let cd = require("commons/client-data")(config["client-data"]);
-        module.exports.clientData = cd;
-        module.exports.logger = bunyan.createLogger({
+        for (let key in config) {
+            if (process.env[key]) {
+                try {
+                    config[key] = JSON.parse(process.env[key]);
+                } catch (e) {
+                    config[key] = process.env[key];
+                }
+            }
+        }
+
+        const log = bunyan.createLogger({
             name: "pusher",
             level: config.log_level
         });
+
+        log.info({config});
+
+        module.exports.config = config;
+        let cd = require("commons/client-data")(config["client-data"]);
+        module.exports.clientData = cd;
+        module.exports.logger = log;
         let promises = [];
         if (config.email_transport === "sendgrid") {
             let sg = cd.getGraphConfig().then(conf =>
